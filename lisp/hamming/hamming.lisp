21c1a5a3900af58be51ea8f861d05a1a76300daf
jaccarmac <jaccarmac@gmail.com>
2016-12-21 06:11:44 +0000

    Add Hamming distance function.
    FossilOrigin-Name: 4240a668c37d239408b0682f647992d96d9e193c

--- lisp/hamming/hamming.lisp
+++ lisp/hamming/hamming.lisp
@@ -6,4 +6,9 @@
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
-  )
+  (if (= (length dna1) (length dna2))
+      (reduce (lambda (diffs pair)
+                (if (eq (car pair) (cdr pair)) diffs (1+ diffs)))
+              (map 'list #'cons dna1 dna2)
+              :initial-value 0)
+      nil))
