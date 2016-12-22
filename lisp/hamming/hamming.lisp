12fbf2399048f21f9a2fd8d7ca86c097c26eaba7
jaccarmac <jaccarmac@gmail.com>
2016-12-22 00:39:40 +0000

    Fix indentation.
    FossilOrigin-Name: 098b8f285de57f82f3b7ece35ebb7c19ae6998cb

--- lisp/hamming/hamming.lisp
+++ lisp/hamming/hamming.lisp
@@ -7,7 +7,7 @@
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
   (when (= (length dna1) (length dna2))
-      (reduce (lambda (diffs pair)
-                (if (eq (car pair) (cdr pair)) diffs (1+ diffs)))
-              (map 'list #'cons dna1 dna2)
-              :initial-value 0)))
+    (reduce (lambda (diffs pair)
+              (if (eq (car pair) (cdr pair)) diffs (1+ diffs)))
+            (map 'list #'cons dna1 dna2)
+            :initial-value 0)))
