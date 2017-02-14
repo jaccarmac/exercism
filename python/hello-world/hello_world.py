e732fcfd9188eec2fdca2f5bdba9c6edea7fba16
jaccarmac <jaccarmac@gmail.com>
2017-02-14 20:30:30 +0000

    Finish Python Hello problem.
    FossilOrigin-Name: de65cfed60b4d6747f2ee351460183ba3952286c

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -3,5 +3,8 @@
 #
 
 
-def hello(name=''):
-    return
+def hello(name=""):
+    if name is not None and len(name) is not 0:
+        return "Hello, " + name + "!"
+    else:
+        return "Hello, World!"
