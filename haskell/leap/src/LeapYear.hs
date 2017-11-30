12f6a87163fee1e3afbb5a0b311b86c443063b7d
jaccarmac <jaccarmac@gmail.com>
2017-11-30 23:29:56 +0000

    Simplify inner function by capturing.
    FossilOrigin-Name: ffca271e5da462977c0b2cd8c3ca178fab309d3c8dbfc5f4e74ccc58a92aee04

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,8 +1,6 @@
 module LeapYear (isLeapYear) where
 
-divisibleBy :: Integer -> Integer -> Bool
-divisibleBy dividend divisor = mod dividend divisor == 0
-
 isLeapYear :: Integer -> Bool
-isLeapYear year = or [ and [divisibleBy year 4, not (divisibleBy year 100)]
-                     , divisibleBy year 400]
+isLeapYear year = or [ and [yearDivisibleBy 4, not (yearDivisibleBy 100)]
+                     , yearDivisibleBy 400]
+                  where yearDivisibleBy divisor = year `mod` divisor == 0
