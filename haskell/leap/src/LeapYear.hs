6df1aa0cce665ea307959c04f7b3740119a1c188
jaccarmac <jaccarmac@gmail.com>
2017-11-30 22:57:57 +0000

    Fix alignment.
    FossilOrigin-Name: 0cac127d29171c60393f26aab0e2bfbfc40bf4bf49282b6d81308cbfc7a2d25e

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -4,5 +4,5 @@ divisibleBy :: Integer -> Integer -> Bool
 divisibleBy dividend divisor = mod dividend divisor == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = or [and [divisibleBy year 4, not (divisibleBy year 100)]
+isLeapYear year = or [ and [divisibleBy year 4, not (divisibleBy year 100)]
                      , divisibleBy year 400]
