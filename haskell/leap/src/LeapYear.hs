0dfbf365d4ca98121cbec192bfb97c2429f537c1
jaccarmac <jaccarmac@gmail.com>
2017-11-30 22:33:04 +0000

    Add solution modeled on my Lisp solution.
    FossilOrigin-Name: 06b7e3c7d662d3ac6a220351abea2abee9878667e4912de6504eb52ea268a331

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,7 @@
 module LeapYear (isLeapYear) where
 
+divisibleBy :: Integer -> Integer -> Bool
+divisibleBy dividend divisor = mod dividend divisor == 0
+
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year = or [and [divisibleBy year 4, not (divisibleBy year 100)], divisibleBy year 400]
