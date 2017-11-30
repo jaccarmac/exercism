5ab522185c30b59d710c78bf9401971104872f2b
jaccarmac <jaccarmac@gmail.com>
2017-11-30 23:35:32 +0000

    Another alignment tweak.
    FossilOrigin-Name: 5d819b76ed66bfe44fb0a00000fdd1c7e7e5b316ce44441d7ceaa9016e643fd7

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,5 +2,5 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year = or [ and [yearDivisibleBy 4, not (yearDivisibleBy 100)]
-                     , yearDivisibleBy 400]
+                     , yearDivisibleBy 400                                ]
                   where yearDivisibleBy divisor = year `mod` divisor == 0
