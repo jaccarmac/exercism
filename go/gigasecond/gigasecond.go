b194562a3238bfd18d5fbbbfc52eeed748e0565c
jaccarmac <jaccarmac@gmail.com>
2017-12-04 22:01:10 +0000

    Simplify constant for second multiplication.
    FossilOrigin-Name: b58e09791250c8a2d3cc9fa6b02efe4f8187b731b6a80cce0e4b8cca99584336

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -5,5 +5,5 @@ import "time"
 
 // AddGigasecond returns a time.Time a gigasecond after the given one.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Duration(1000000000) * time.Second)
+	return t.Add(1e9 * time.Second)
 }
