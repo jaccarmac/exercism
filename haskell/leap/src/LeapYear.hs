2d0dc746781a3ebeeeb7616b4b1f3864b9fa10f8
jaccarmac <jaccarmac@gmail.com>
2017-12-04 12:02:12 +0000

    Use proper conditionals.
    FossilOrigin-Name: 3d21a15e3335245fa1f57e065578732bbc0331d93c1c66d21c2db350eba464eb

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,6 +1,5 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = or [ and [yearDivisibleBy 4, not (yearDivisibleBy 100)]
-                     , yearDivisibleBy 400                                ]
-                  where yearDivisibleBy divisor = year `mod` divisor == 0
+isLeapYear year = yearDivisibleBy 4 && not (yearDivisibleBy 100) || yearDivisibleBy 400
+  where yearDivisibleBy divisor = year `mod` divisor == 0
