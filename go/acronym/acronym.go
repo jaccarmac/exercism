854e4426f34a74de8236b4cc277a7914eb7111ca
jaccarmac <jaccarmac@gmail.com>
2017-12-07 01:36:05 +0000

    Implement acronym creator in Go, based on Rob Pike's state machine.
    FossilOrigin-Name: ce9ebe39c87f66e4637f70e8e5555fd7448c47f13cf33a5821476053edc9e7ec

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,17 +1,72 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package acronym creates acronyms from long names.
 package acronym
 
-func Abbreviate(string) string
+import (
+	"strings"
+	"unicode"
+)
 
-// Abbreviate should have a comment documenting it.
+type stateFn func(*phraseState) stateFn
+
+type phraseState struct {
+	phrase  []rune
+	acronym string
+}
+
+// Abbreviate returns the acronym for a long string.
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	thisPhraseState := createPhraseState(s)
+	for state := startState; state != nil; {
+		state = state(&thisPhraseState)
+	}
+	return thisPhraseState.acronym
+}
+
+func createPhraseState(phrase string) phraseState {
+	return phraseState{[]rune(phrase), ""}
+}
+
+func startState(state *phraseState) stateFn {
+	if len(state.phrase) == 0 {
+		return nil
+	}
+	thisRune := state.phrase[0]
+	if unicode.IsLetter(thisRune) {
+		return startOfWordState
+	}
+	return skipSpaceState
+}
+
+func startOfWordState(state *phraseState) stateFn {
+	thisRune := state.phrase[0]
+	state.phrase = state.phrase[1:]
+	state.acronym = state.acronym + strings.ToUpper(string(thisRune))
+	if len(state.phrase) == 0 {
+		return nil
+	}
+	return skipUntilSpaceState
+}
+
+func skipSpaceState(state *phraseState) stateFn {
+	thisRune := state.phrase[0]
+	if unicode.IsLetter(thisRune) {
+		return startOfWordState
+	}
+	state.phrase = state.phrase[1:]
+	if len(state.phrase) == 0 {
+		return nil
+	}
+	return skipSpaceState
+}
+
+func skipUntilSpaceState(state *phraseState) stateFn {
+	thisRune := state.phrase[0]
+	if !unicode.IsLetter(thisRune) {
+		return skipSpaceState
+	}
+	state.phrase = state.phrase[1:]
+	if len(state.phrase) == 0 {
+		return nil
+	}
+	return skipUntilSpaceState
 }
