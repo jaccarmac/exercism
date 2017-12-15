707780cca1368fdcf59fd10e4b36d0180b33005c
jaccarmac <jaccarmac@gmail.com>
2017-12-15 18:36:32 +0000

    Be more disciplined about conditions.
    FossilOrigin-Name: 892ff343cc70cda21d085ae9c8d574a69caff73dbec04961b9d61dfb00488661

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -2,6 +2,6 @@ def is_isogram(string):
     letters = set()
     for letter in string.lower():
         if letter in (" ", "-"): continue
-        if letter in letters: return False
-        letters.add(letter)
+        elif letter in letters: return False
+        else: letters.add(letter)
     return True
