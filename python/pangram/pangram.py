1fa2a661b541b061fe8d47bf5e9259a1a6eca346
jaccarmac <jaccarmac@gmail.com>
2017-12-15 18:50:59 +0000

    Solve pangram in Python.
    FossilOrigin-Name: e85a01f0162fb03365c075743b9f7243be8d967c5049c4accf7c03798735222f

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,4 @@
 def is_pangram(sentence):
-    pass
+    english_letters = {letter for letter in "abdefghijklmnopqrstuvwxyz"}
+    sentence_letters = {letter for letter in sentence.lower()}
+    return english_letters.intersection(sentence_letters) == english_letters
