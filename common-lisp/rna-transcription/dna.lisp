cfeea4a3e19b9e988062e65e9c054270c89d3c77
jaccarmac <jaccarmac@gmail.com>
2019-02-18 09:38:36 +0000

    More LOOP madness.
    FossilOrigin-Name: 0ce6f89f7664cf8927a6075e3dce58a2a49182098f671b2a0c77346b390fcd83

--- common-lisp/rna-transcription/dna.lisp
+++ common-lisp/rna-transcription/dna.lisp
@@ -6,4 +6,11 @@
 
 (defun to-rna (str)
   "Transcribe a string representing DNA nucleotides to RNA."
-  )
+  (format nil "~{~a~}"
+          (loop for nucleotide across str
+             collect (case nucleotide
+                       (#\C #\G)
+                       (#\G #\C)
+                       (#\A #\U)
+                       (#\T #\A)
+                       (otherwise (error "~A is not a valid nucleotide" nucleotide))))))
