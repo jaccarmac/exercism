1e484ffd705858228e81bec368b1eea94c6df775
jaccarmac <jaccarmac@gmail.com>
2019-02-18 13:11:32 +0000

    Lotsa regex solution for the Python acronym problem.
    FossilOrigin-Name: 0e49cc709b68c249eda2806b1ab10c98e77c495fff77ff3e9c0dae4edda7d046

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,11 @@
+import re
+
 def abbreviate(words):
-    pass
+    return ''.join(capitalize_first(word) for word in abbrev_words(words))
+
+def capitalize_first(word):
+    first_letter = re.search(r'[a-zA-Z]', word)
+    return first_letter[0].upper() if first_letter else ''
+
+def abbrev_words(words):
+    return re.split(r' |-', words)
