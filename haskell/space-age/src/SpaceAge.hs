ce8fe204a9d3a2f4e35c47c93c66d80da2e5b422
jaccarmac <jaccarmac@gmail.com>
2019-02-21 08:07:47 +0000

    Nice infix operators.
    FossilOrigin-Name: 16c9301bfc410f131436715c4f3a7db777f3c11041d0495c3778b80aea6a5fe2

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -16,11 +16,11 @@ ageOnEarth :: Float -> Float
 ageOnEarth seconds = seconds / earthYear
 
 ageOn :: Planet -> Float -> Float
-ageOn Mercury = (\x -> x / 0.2408467) . ageOnEarth
-ageOn Venus = (\x -> x / 0.61519726) . ageOnEarth
+ageOn Mercury = (/ 0.2408467) . ageOnEarth
+ageOn Venus = (/ 0.61519726) . ageOnEarth
 ageOn Earth = ageOnEarth
-ageOn Mars = (\x -> x / 1.8808158) . ageOnEarth
-ageOn Jupiter = (\x -> x / 11.862615) . ageOnEarth
-ageOn Saturn = (\x -> x / 29.447498) . ageOnEarth
-ageOn Uranus = (\x -> x / 84.016846) . ageOnEarth
-ageOn Neptune = (\x -> x / 164.79132) . ageOnEarth
+ageOn Mars = (/ 1.8808158) . ageOnEarth
+ageOn Jupiter = (/ 11.862615) . ageOnEarth
+ageOn Saturn = (/ 29.447498) . ageOnEarth
+ageOn Uranus = (/ 84.016846) . ageOnEarth
+ageOn Neptune = (/ 164.79132) . ageOnEarth
