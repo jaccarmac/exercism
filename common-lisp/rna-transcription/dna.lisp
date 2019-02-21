8a8d4053800a5a93b31a94074d24e1c5df14ec2f
jaccarmac <jaccarmac@gmail.com>
2019-02-21 23:05:11 +0000

    Dynamic variable, anaphoric LOOP clause.
    FossilOrigin-Name: 7a66c9e38e7e46b38ca50c495bb28a81497108dda898962b9d4c8c88e9e643fe

--- common-lisp/rna-transcription/dna.lisp
+++ common-lisp/rna-transcription/dna.lisp
@@ -4,12 +4,16 @@
   (:export #:to-rna))
 (in-package #:dna)
 
+(defparameter *dna-to-rna-nucleotides*
+  '((#\C . #\G)
+    (#\G . #\C)
+    (#\A . #\U)
+    (#\T . #\A))
+  "Alist of nucleotide translations, DNA first.")
+
 (defun to-rna (str)
   "Transcribe a string representing DNA nucleotides to RNA."
   (coerce (loop for nucleotide across str
-             collect (ecase nucleotide
-                       (#\C #\G)
-                       (#\G #\C)
-                       (#\A #\U)
-                       (#\T #\A)))
+             if (cdr (assoc nucleotide *dna-to-rna-nucleotides*)) collect it
+             else do (error "~A is not a valid nucleotide" nucleotide))
           'string))
