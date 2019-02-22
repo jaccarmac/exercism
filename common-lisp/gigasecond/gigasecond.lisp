7f2a90a88a7bf12bffbf20e0bed5a98eaeb10028
jaccarmac <jaccarmac@gmail.com>
2019-02-22 06:00:30 +0000

    Gigasecond in CL.
    FossilOrigin-Name: 1ef4b2e868b2167e4efa9a55607d81223467001f68eec9a4bbef05b961cca966

--- common-lisp/gigasecond/gigasecond.lisp
+++ common-lisp/gigasecond/gigasecond.lisp
@@ -4,4 +4,10 @@
   (:export #:from))
 (in-package #:gigasecond)
 
-(defun from (year month day hour minute second))
+(defun from (year month day hour minute second)
+  (multiple-value-bind
+	(second minute hour day month year)
+      (decode-universal-time
+       (+ 1000000000
+          (encode-universal-time second minute hour day month year 0)) 0)
+    (list year month day hour minute second)))
