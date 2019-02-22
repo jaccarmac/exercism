7fcbcb822fb3504b6468890d1e936063070592ca
jaccarmac <jaccarmac@gmail.com>
2019-02-22 07:00:56 +0000

    Ugh I htae myself for using LOOP so much.
    FossilOrigin-Name: be99edc1fca99f17e0af81df36788a349cb68a7bc7282cb7547a391b393b5719

--- common-lisp/word-count/word-count.lisp
+++ common-lisp/word-count/word-count.lisp
@@ -3,3 +3,22 @@
   (:use #:cl)
   (:export #:word-count))
 (in-package #:phrase)
+
+(defun inc-word-count (word words)
+  (let ((new-words (remove word words :key #'car :test #'string=))
+        (new-count (1+ (or (cdr (assoc word words :test #'string=)) 0))))
+    (acons word new-count new-words)))
+
+(defun word-count (phrase)
+  (loop for letter across phrase
+     with words
+     with word = ""
+     for word-ready-p = (not (zerop (length word)))
+     if (not (alphanumericp letter))
+       when word-ready-p
+       do (progn (setf words (inc-word-count word words))
+                 (setf word ""))
+       end
+     else
+     do (setf word (concatenate 'string word (list (char-downcase letter))))
+     finally (return (if word-ready-p (inc-word-count word words) words))))
