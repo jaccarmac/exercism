7c3a9338db10eb8705f7ead3c34f92532cb1815b
jaccarmac <jaccarmac@gmail.com>
2019-02-23 11:53:55 +0000

    Link to Factor bootstrapping info.
    FossilOrigin-Name: 93f832e4add8971e789263cc0a54262c160b4eb86975abf7a004c778f907a786

--- factor/guix-manifest.scm
+++ factor/guix-manifest.scm
@@ -1,8 +1,12 @@
 ;; Two reasons.
-;; 
+;;
 ;; 1. Factor isn't in the Guix package store.
-;; 
+;;
 ;; 2. It requires an image for bootstrapping, so I may need to write a looooong
 ;; bootstrap chain a la Rust to get it working. File that under TODO.
+;;
+;; https://factor-language.blogspot.com/2010/01/factors-bootstrap-process-explained.html
+;;
+;; still accurate?
 
 (specifications->manifest '())
