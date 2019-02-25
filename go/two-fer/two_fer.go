fefa30f9460971c3461238f397f1e1be6138a0ee
jaccarmac <jaccarmac@gmail.com>
2019-02-25 01:51:44 +0000

    Add comments for Go two-fer.
    FossilOrigin-Name: 92774a21983d97d7904ba35ac4b4031eefa6bee062cc351918ddef2252574a28

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,11 +1,8 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer generates a custom phrase based on a name.
 package twofer
 
-// ShareWith should have a comment documenting it.
+// ShareWith generates a phrase with an optional name spliced in.
+// To use a default instead of a name, pass the empty string.
 func ShareWith(optionalName string) string {
 	name := optionalName
 	if name == "" {
