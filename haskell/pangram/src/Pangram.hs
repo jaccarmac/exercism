596843a9453ac5ea50712e5bd3439009e9052b34
jaccarmac <jaccarmac@gmail.com>
2019-02-25 19:04:19 +0000

    Figure out if a word is a pangram.
    FossilOrigin-Name: 0e61ebcf319e0c3795f2b67812f5d1f84c4d401d191eb52bda034602733b2529

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,13 @@
 module Pangram (isPangram) where
 
+import           Data.Char (toLower)
+import           Data.Set  (Set, fromList, isSubsetOf)
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram = (letters `isSubsetOf`) . lettersIn
+
+letters :: Set Char
+letters = fromList ['a'..'z']
+
+lettersIn :: String -> Set Char
+lettersIn = fromList . map toLower
