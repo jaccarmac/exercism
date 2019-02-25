59f30e551ea3785a55c02a51ffac0330a830f2df
jaccarmac <jaccarmac@gmail.com>
2019-02-25 18:48:46 +0000

    Styling + stylish-haskell.
    FossilOrigin-Name: 7fa38da742f3ae370158e6e3c7a045f9433d9588cf9795516076294b5e669d16

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,14 +1,16 @@
 module Bob (responseFor) where
 
-import Data.Text (empty, last, pack, Text, toLower, toUpper, strip)
+import           Data.Text (Text, empty, last, pack, strip, toLower, toUpper)
 
 responseFor :: String -> String
-responseFor s = let respond t 
-                      | isSilence t = "Fine. Be that way!"
-                      | isShouting t && isQuestion t = "Calm down, I know what I'm doing!"
-                      | isShouting t = "Whoa, chill out!"
-                      | isQuestion t = "Sure."
-                      | otherwise = "Whatever." in respond $ strip $ pack s
+responseFor s = let
+  respond t
+    | isSilence t = "Fine. Be that way!"
+    | isShouting t && isQuestion t = "Calm down, I know what I'm doing!"
+    | isShouting t = "Whoa, chill out!"
+    | isQuestion t = "Sure."
+    | otherwise = "Whatever."
+  in respond $ strip $ pack s
 
 isShouting :: Text -> Bool
 isShouting t = t == toUpper t && t /= toLower t
