36e300b8d85cea67e2820da1e3ae205ad44f8df3
jaccarmac <jaccarmac@gmail.com>
2019-02-25 16:59:12 +0000

    Implement Bob. The indentation seems... off.
    FossilOrigin-Name: ee2e1ba378d77bbc8025163899fc99c78fec8d7fe8e75d3fccf8733f943ba471

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Bob (responseFor) where
 
+import Data.Text (empty, last, pack, Text, toLower, toUpper, strip)
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor s = (\t -> if
+                    | isSilence t -> "Fine. Be that way!"
+                    | isShouting t && isQuestion t -> "Calm down, I know what I'm doing!"
+                    | isShouting t -> "Whoa, chill out!"
+                    | isQuestion t -> "Sure."
+                    | otherwise -> "Whatever.") $ strip $ pack s
+
+isShouting :: Text -> Bool
+isShouting t = t == toUpper t && t /= toLower t
+
+isQuestion :: Text -> Bool
+isQuestion = (== '?') . Data.Text.last
+
+isSilence :: Text -> Bool
+isSilence = (== empty)
