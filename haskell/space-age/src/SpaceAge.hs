dc1d3409bcde681f0c870ebf64c3f4a7a917aa44
jaccarmac <jaccarmac@gmail.com>
2019-02-25 01:47:45 +0000

    Less point-free solution to space-age in Haskell.
    FossilOrigin-Name: c778d7db3b155d305ce8f6b1c50518809a49c427edf664d6d4fa1c302a450a8f

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -9,21 +9,18 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
-earthYear :: Float
-earthYear = 31557600
+ageOn :: Planet -> Float -> Float
+ageOn planet seconds = ageOnEarth seconds / yearInEarthYears planet
 
 ageOnEarth :: Float -> Float
-ageOnEarth = (/ earthYear)
+ageOnEarth = (/ 31557600)
 
-ageOn :: Planet -> Float -> Float
-ageOn = (ageOnEarth .) . convertToEarthTime
-
-convertToEarthTime :: Planet -> Float -> Float
-convertToEarthTime Mercury = (/ 0.2408467)
-convertToEarthTime Venus = (/ 0.61519726)
-convertToEarthTime Earth = id
-convertToEarthTime Mars = (/ 1.8808158)
-convertToEarthTime Jupiter = (/ 11.862615)
-convertToEarthTime Saturn = (/ 29.447498)
-convertToEarthTime Uranus = (/ 84.016846)
-convertToEarthTime Neptune = (/ 164.79132)
+yearInEarthYears :: Planet -> Float
+yearInEarthYears Mercury = 0.2408467
+yearInEarthYears Venus = 0.61519726
+yearInEarthYears Earth = 1
+yearInEarthYears Mars = 1.8808158
+yearInEarthYears Jupiter = 11.862615
+yearInEarthYears Saturn = 29.447498
+yearInEarthYears Uranus = 84.016846
+yearInEarthYears Neptune = 164.79132
