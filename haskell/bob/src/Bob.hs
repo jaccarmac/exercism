d266b0011864365c3123ed700602c52fec676dd9
jaccarmac <jaccarmac@gmail.com>
2019-02-25 18:54:21 +0000

    Don't need s, $ -> .
    FossilOrigin-Name: a4fa686feb82fa487c6b8c72a8deebd0120ff5afead93c43de8dc0acbabd6f31

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -3,14 +3,14 @@ module Bob (responseFor) where
 import           Data.Text (Text, empty, last, pack, strip, toLower, toUpper)
 
 responseFor :: String -> String
-responseFor s = let
+responseFor = let
   respond t
     | isSilence t = "Fine. Be that way!"
     | isShouting t && isQuestion t = "Calm down, I know what I'm doing!"
     | isShouting t = "Whoa, chill out!"
     | isQuestion t = "Sure."
     | otherwise = "Whatever."
-  in respond $ strip $ pack s
+  in respond . strip . pack
 
 isShouting :: Text -> Bool
 isShouting t = t == toUpper t && t /= toLower t
