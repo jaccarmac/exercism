9388abc7b69d00ef56f3422c6249654d660c3667
jaccarmac <jaccarmac@gmail.com>
2019-02-25 02:11:28 +0000

    Update CL word-count solution.
    FossilOrigin-Name: ed93d38e5844d4654ab29296ca85e5bdf7d367f6b3b42882eac3e333f036f455

--- common-lisp/word-count/word-count.lisp
+++ common-lisp/word-count/word-count.lisp
@@ -9,16 +9,20 @@
         (new-count (1+ (or (cdr (assoc word words :test #'string=)) 0))))
     (acons word new-count new-words)))
 
-(defun word-count (phrase)
+(defun words-in (phrase)
   (loop for letter across phrase
-     with words
      with word = ""
      for word-ready-p = (not (zerop (length word)))
      if (not (alphanumericp letter))
        when word-ready-p
-       do (progn (setf words (inc-word-count word words))
-                 (setf word ""))
+       collect word into words
+       and do (setf word "")
        end
-     else
-     do (setf word (concatenate 'string word (list (char-downcase letter))))
-     finally (return (if word-ready-p (inc-word-count word words) words))))
+     else do (setf word (concatenate 'string word (list (char-downcase letter))))
+     finally (return (if word-ready-p (cons word words) words))))
+
+(defun word-count (phrase)
+  (loop for word in (words-in phrase)
+     with words
+     do (setf words (inc-word-count word words))
+     finally (return words)))
