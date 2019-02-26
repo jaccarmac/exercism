679811f727f330b587176c5797cbff76b907f5fe
jaccarmac <jaccarmac@gmail.com>
2019-02-26 12:29:56 +0000

    Fix some lints.
    FossilOrigin-Name: 95219bea7810d4d5225fcd4a84352f80a63a475022fca6d1ee57995bc4428a29

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,11 +1,11 @@
 module Bob (responseFor) where
 
-import           Data.Char (isLetter, isSpace, toUpper)
+import           Data.Char (isLetter, isLower, isSpace)
 
 responseFor :: String -> String
 responseFor = let
   respond t
-    | isSilence t = "Fine. Be that way!"
+    | null t = "Fine. Be that way!"
     | isShouting t && isQuestion t = "Calm down, I know what I'm doing!"
     | isShouting t = "Whoa, chill out!"
     | isQuestion t = "Sure."
@@ -13,13 +13,10 @@ responseFor = let
   in respond . strip
 
 isShouting :: String -> Bool
-isShouting s = any isLetter s && all (\c -> c == toUpper c) s
+isShouting s = any isLetter s && not (any isLower s)
 
 isQuestion :: String -> Bool
 isQuestion = (== '?') . last
-
-isSilence :: String -> Bool
-isSilence = (== 0) . length
 
 strip :: String -> String
 strip = filter (not . isSpace)
