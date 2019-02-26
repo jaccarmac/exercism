0897f32e5bb7c912aebbdcc2291ffdf99705a342
jaccarmac <jaccarmac@gmail.com>
2019-02-26 12:22:33 +0000

    Dependency-less Pangram solution.
    FossilOrigin-Name: 6339247275aa9b70092c92ad7b2ff0635b66215dd35db587ed510d3b637f987b

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,13 +1,9 @@
 module Pangram (isPangram) where
 
 import           Data.Char (toLower)
-import           Data.Set  (Set, fromList, isSubsetOf)
 
 isPangram :: String -> Bool
-isPangram = (letters `isSubsetOf`) . lettersIn
+isPangram s = all (`elem` map toLower s) letters
 
-letters :: Set Char
-letters = fromList ['a'..'z']
-
-lettersIn :: String -> Set Char
-lettersIn = fromList . map toLower
+letters :: [Char]
+letters = ['a'..'z']
