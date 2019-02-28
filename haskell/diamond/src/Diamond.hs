c7451e3a8ddfc0911aa0718582b71bf40499b0c3
jaccarmac <jaccarmac@gmail.com>
2019-02-28 12:09:46 +0000

    Diamond solution thing.
    FossilOrigin-Name: a0fd3b074f57fa9f4e11d8bfdafe5a729e2fb205f25bfc13481261c8efaed9dd

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -1,4 +1,28 @@
 module Diamond (diamond) where
 
+import           Data.Char (chr, ord)
+
 diamond :: Char -> Maybe [String]
-diamond = error "You need to implement this function"
+diamond c = Just $ mirrorRows $ padRows (diamondRowsFrom c) maxDepth
+  where maxDepth = ord c - ord 'A'
+
+mirrorRows :: [String] -> [String]
+mirrorRows []     = []
+mirrorRows (s:[]) = [s]
+mirrorRows (s:ss) = s:mirrorRows ss ++ [s]
+
+padRows :: [String] -> Int -> [String]
+padRows [] _ = []
+padRows ss 0 = ss
+padRows (s:ss) depth = (pad ++ s ++ pad):(padRows ss $ depth - 1)
+  where pad = (take depth $ repeat ' ')
+
+diamondRowsFrom :: Char -> [String]
+diamondRowsFrom 'A' = ["A"]
+diamondRowsFrom c = (diamondRowsFrom $ chr $ ord c - 1) ++ [currentRow]
+  where currentRow = diamondRow c $ ord c - ord 'A'
+
+diamondRow :: Char -> Int -> String
+diamondRow c 0     = [c]
+diamondRow c depth = c:(take spaces $ repeat ' ') ++ [c]
+  where spaces = depth * 2 - 1
