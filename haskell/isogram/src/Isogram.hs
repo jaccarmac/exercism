58a10c8d34acba89799154e52983b2c6214b8968
jaccarmac <jaccarmac@gmail.com>
2019-02-28 11:26:37 +0000

    Implement Haskell isogram check.
    FossilOrigin-Name: 112cd45882c880993d05d5a69ec9c1879b904ef95d2bd39f4f36ddc151d95c85

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,4 +1,13 @@
 module Isogram (isIsogram) where
 
+import           Data.Char (isLetter, toLower)
+
 isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+isIsogram = checkIsogram ""
+
+checkIsogram :: String -> String -> Bool
+checkIsogram _ "" = True
+checkIsogram seen (c:cs)
+  | isLetter c && lc `elem` seen = False
+  | otherwise = checkIsogram (lc:seen) cs
+  where lc = toLower c
