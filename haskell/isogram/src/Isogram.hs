3d9c447059a0cffcef9e7b063b63c48f2443128e
jaccarmac <jaccarmac@gmail.com>
2019-02-28 11:37:57 +0000

    Don't need a helper function.
    FossilOrigin-Name: 8222bccecc3b1a3eca03b1e9369795a6f666b434bc4d2e973dc58c5d96f67f40

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -3,11 +3,6 @@ module Isogram (isIsogram) where
 import           Data.Char (isLetter, toLower)
 
 isIsogram :: String -> Bool
-isIsogram = checkIsogram ""
-
-checkIsogram :: String -> String -> Bool
-checkIsogram _ "" = True
-checkIsogram seen (c:cs)
-  | isLetter c && lc `elem` seen = False
-  | otherwise = checkIsogram (lc:seen) cs
-  where lc = toLower c
+isIsogram ""     = True
+isIsogram (c:cs) = (not $ toLower c `elem` cs') && isIsogram cs'
+  where cs' = filter isLetter $ map toLower cs
