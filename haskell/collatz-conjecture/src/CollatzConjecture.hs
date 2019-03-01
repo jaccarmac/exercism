2007cf77b60a4465e127a988c76799aa9dd80a9e
jaccarmac <jaccarmac@gmail.com>
2019-03-01 19:03:39 +0000

    indentation
    FossilOrigin-Name: a4dd9d2fec01de57602c0f47c86e49c8606c842f73d77d8c209bda58e82d11c5

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -5,5 +5,9 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz = (fst <$>) . until
   (maybe True ((== 1) . snd))
-  ((\(c, i) -> if i > 1 then Just (succ c,  if even i then i `quot` 2 else 3 * i + 1) else Nothing) =<<)
+  ((\(c, i) ->
+      if i > 1
+      then Just (succ c,  if even i then i `quot` 2 else 3 * i + 1)
+      else Nothing)
+    =<<)
   . Just . (0,)
