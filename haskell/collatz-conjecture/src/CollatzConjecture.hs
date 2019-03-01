f8fb6b57d53f30b16943a11fd1251f929c363e36
jaccarmac <jaccarmac@gmail.com>
2019-03-01 04:11:01 +0000

    simple recursive solution to Collatz.
    FossilOrigin-Name: 73ea2d0aac5dc5094a1480cdf5df2cc38feef1e7a4300af021a2051afb93cc3e

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,10 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz 1 = Just 0
+collatz i
+  | i > 1 = succ <$> collatz
+    (if even i
+     then i `div` 2
+     else 3 * i + 1)
+  | otherwise = Nothing
