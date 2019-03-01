b145e72737ee10874c56997537a975fcbc79a5be
jaccarmac <jaccarmac@gmail.com>
2019-03-01 19:01:27 +0000

    Use monad ops.
    FossilOrigin-Name: 5580ea7a8565f14fdca26918e8f160255c168aac9480303c3f05d34189a7fc73

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE TupleSections #-}
 
 module CollatzConjecture (collatz) where
@@ -6,7 +5,5 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz = (fst <$>) . until
   (maybe True ((== 1) . snd))
-  (\case Nothing -> Nothing
-         Just (c, i) | i > 1 -> Just (succ c,  if even i then i `quot` 2 else 3 * i + 1)
-                     | otherwise  -> Nothing)
+  ((\(c, i) -> if i > 1 then Just (succ c,  if even i then i `quot` 2 else 3 * i + 1) else Nothing) =<<)
   . Just . (0,)
