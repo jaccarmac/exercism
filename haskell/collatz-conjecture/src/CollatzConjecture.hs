d41589a0b44bfe998f2f7ac8ba4fd443e519fd32
jaccarmac <jaccarmac@gmail.com>
2019-03-01 20:37:26 +0000

    One branch in the function only.
    FossilOrigin-Name: 16dde26465b2ce0758eb36fdb65ee45c37f8fee4d8399166fcbef0288ba13f6f

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,8 +1,11 @@
 module CollatzConjecture (collatz) where
 
+import           Data.Foldable (find)
+
 collatz :: Integer -> Maybe Integer
-collatz 1 = Just 0
-collatz n = (\l -> if l > 0 then Just $ toInteger l else Nothing)
+collatz n = find ((|| n == 1) . (>0))
+  $ Just
+  $ toInteger
   $ length
   $ takeWhile (>1)
   $ iterate (\i -> if even i then i `div` 2 else 3 * i + 1) n
