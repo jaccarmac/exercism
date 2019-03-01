ab4deed7b266c380b9e54a8281dff836254dd1ae
jaccarmac <jaccarmac@gmail.com>
2019-03-01 04:16:52 +0000

    hlint isogram
    FossilOrigin-Name: 4374137ea24caa942c7df8804b75dd9988b620ae6b874e5378c3e06efe849f3b

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -4,5 +4,5 @@ import           Data.Char (isLetter, toLower)
 
 isIsogram :: String -> Bool
 isIsogram ""     = True
-isIsogram (c:cs) = (not $ toLower c `elem` cs') && isIsogram cs'
+isIsogram (c:cs) = notElem (toLower c) cs' && isIsogram cs'
   where cs' = filter isLetter $ map toLower cs
