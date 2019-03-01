5067b3e11be9652354182f7274012ac90a0493be
jaccarmac <jaccarmac@gmail.com>
2019-03-01 18:52:56 +0000

    lint and reduce a little
    FossilOrigin-Name: 2774cf21066b0169c50f7ae820635219e3663805873edc56aa9c70efff6b1895

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -4,10 +4,8 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = (fst <$>) . until (\case
-                                  Nothing -> True
-                                  Just (_, i) -> i == 1)
+collatz = (fst <$>) . until (maybe True ((== 1) . snd))
             (\case Nothing -> Nothing
-                   Just (c, i) | i > 1 -> Just ((succ c), (if even i then i `quot` 2 else 3 * i + 1))
+                   Just (c, i) | i > 1 -> Just (succ c,  if even i then i `quot` 2 else 3 * i + 1)
                                | otherwise  -> Nothing)
             . Just . (0,)
