f4da44df8bd0510ecea81cd4a13b03d64f623f97
jaccarmac <jaccarmac@gmail.com>
2019-04-24 19:46:21 +0000

    Elisp Bob implementation.
    FossilOrigin-Name: ccdbdcb5a55874ebbd303b304abc075442ff36a2640bd851465af53069523628

--- elisp/bob/bob.el
+++ elisp/bob/bob.el
@@ -1,9 +1,28 @@
 ;;; bob.el --- Bob exercise (exercism)
 
-;;; Commentary:
+;;; Commentary: Bob responds to various sentences.
 
 ;;; Code:
+(eval-when-compile (require 'subr-x))
 
+(defun response-for (sentence)
+  (let ((sentence (string-trim sentence)))
+    (cond ((saying-nothing-p sentence) "Fine. Be that way!")
+          ((and (yellingp sentence) (askingp sentence))
+           "Calm down, I know what I'm doing!")
+          ((yellingp sentence) "Whoa, chill out!")
+          ((askingp sentence) "Sure.")
+          (t "Whatever."))))
+
+(defun saying-nothing-p (sentence)
+  (= (length sentence) 0))
+
+(defun yellingp (sentence)
+  (and (string-match-p "[[:alpha:]]" sentence)
+       (string-equal sentence (upcase sentence))))
+
+(defun askingp (sentence)
+  (string-suffix-p "?" sentence))
 
 (provide 'bob)
 ;;; bob.el ends here
