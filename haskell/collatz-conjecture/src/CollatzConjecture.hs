258b44cfe5b0f40cec72a22df82908d1744c41b7
jaccarmac <jaccarmac@gmail.com>
2019-04-24 19:20:42 +0000

    Add badly-named case function.
    FossilOrigin-Name: d3f47d175d7d365871ceb76d9a2dbd89aa232e6d39255708a8f5208f937d1dd1

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,8 +1,11 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = (\i -> if n == 1 && i == 0 then Just 0 else if i == 0 then Nothing else Just i)
+collatz n = ifNothing (if n == 1 then Just 0 else Nothing)
+  $ (\i -> if i == 0 then Nothing else Just i)
   $ toInteger
   $ length
   $ takeWhile (>1)
   $ iterate (\i -> if even i then i `quot` 2 else 3 * i + 1) n
+  where ifNothing d Nothing = d
+        ifNothing _ m       = m
