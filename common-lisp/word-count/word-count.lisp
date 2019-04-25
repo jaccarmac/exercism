1538a8b1d5ae4c782816dca777fd9a0bb901bd1c
jaccarmac <jaccarmac@gmail.com>
2019-04-25 14:53:14 +0000

    Change the way the word count list is cleaned.
    FossilOrigin-Name: 4abc3dfe95595aebe0a48fa4a95df56802c54ffc69361ae0a835b4079c9f8615

--- common-lisp/word-count/word-count.lisp
+++ common-lisp/word-count/word-count.lisp
@@ -5,9 +5,16 @@
 (in-package #:phrase)
 
 (defun inc-word-count (words word)
-  (let ((new-words (remove word words :key #'car :test #'string=))
-        (new-count (1+ (or (cdr (assoc word words :test #'string=)) 0))))
-    (acons word new-count new-words)))
+  (let ((new-count (1+ (or (cdr (assoc word words :test #'string=)) 0))))
+    (acons word new-count words)))
+
+(defun clean-word-counts (counts)
+  (reduce (lambda (new-counts count)
+            (if (assoc (car count) new-counts :test #'string=)
+                new-counts
+                (cons count new-counts)))
+          counts
+          :initial-value nil))
 
 (defun words-in (phrase)
   (loop for letter across phrase
@@ -22,4 +29,4 @@
      finally (return (if word-ready-p (cons word words) words))))
 
 (defun word-count (phrase)
-  (reduce #'inc-word-count (words-in phrase) :initial-value nil))
+  (clean-word-counts (reduce #'inc-word-count (words-in phrase) :initial-value nil)))
