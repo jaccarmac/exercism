5d72ecee3ea9b82d3a5ad34f5a32e2a9381aa85e
jaccarmac <jaccarmac@gmail.com>
2019-04-25 17:44:44 +0000

    Refactor the Common Lisp word count slightly.
    FossilOrigin-Name: 9d087ece54a9be99e7b3e58eee2bf8ce38586921ddc12e45c1c95113c4fc0f62

--- common-lisp/word-count/word-count.lisp
+++ common-lisp/word-count/word-count.lisp
@@ -4,18 +4,6 @@
   (:export #:word-count))
 (in-package #:phrase)
 
-(defun inc-word-count (words word)
-  (let ((new-count (1+ (or (cdr (assoc word words :test #'string=)) 0))))
-    (acons word new-count words)))
-
-(defun clean-word-counts (counts)
-  (reduce (lambda (new-counts count)
-            (if (assoc (car count) new-counts :test #'string=)
-                new-counts
-                (cons count new-counts)))
-          counts
-          :initial-value nil))
-
 (defun words-in (phrase)
   (loop for letter across phrase
      with word = ""
@@ -28,5 +16,16 @@
      else do (setf word (concatenate 'string word (list (char-downcase letter))))
      finally (return (if word-ready-p (cons word words) words))))
 
+(defun inc-word-counts (counts word)
+  (let ((new-count (1+ (or (cdr (assoc word counts :test #'string=)) 0))))
+    (acons word new-count counts)))
+
+(defun add-count-if-not-present (counts count)
+  (if (assoc (car count) counts :test #'string=)
+      counts
+      (cons count counts)))
+
 (defun word-count (phrase)
-  (clean-word-counts (reduce #'inc-word-count (words-in phrase) :initial-value nil)))
+  (reduce #'add-count-if-not-present
+          (reduce #'inc-word-counts (words-in phrase) :initial-value nil)
+          :initial-value nil))
