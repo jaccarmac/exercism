0e054ad80a684668c9457150b36df530b54e6dbb
jaccarmac <jaccarmac@gmail.com>
2019-04-25 14:21:48 +0000

    Implement Haskell RNA transcription.
    FossilOrigin-Name: 0563b380e89f70268aaee4436056f45088e6a74d55d31ba1602f74b3d232cba6

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,12 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA ""     = Right ""
+toRNA (n:ns) = complement n >>= (\c -> fmap ([c]++) (toRNA ns))
+
+complement :: Char -> Either Char Char
+complement 'G' = Right 'C'
+complement 'C' = Right 'G'
+complement 'T' = Right 'A'
+complement 'A' = Right 'U'
+complement c   = Left c
