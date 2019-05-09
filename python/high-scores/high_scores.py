0dd8aa79c930abf926eafc38b43c4c9f4d5aec34
jaccarmac <jaccarmac@gmail.com>
2019-05-09 20:25:28 +0000

    Solve Python High Score.
    FossilOrigin-Name: 5b7bae7b71b2f95b0a659b0926b6764bfbb466153193e0bdf51d946194fed33c

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,17 @@
+from functools import reduce
+
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    def top_three_reducer(top, score):
+        if len(top) < 3 or score > top[-1]:
+            return sorted(top + [score], reverse=True)[:3]
+        else:
+            return top
+    return reduce(top_three_reducer, scores, [])
