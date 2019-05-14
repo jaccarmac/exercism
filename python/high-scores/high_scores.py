65484058150ef5c9fb4e78d33600435a496daad7
jaccarmac <jaccarmac@gmail.com>
2019-05-14 03:04:48 +0000

    Flatten conditional.
    FossilOrigin-Name: 83c3dbf87dfad360fa97a4ca956c1de944e498caa4c75f7228010e93ae7fa866

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -10,8 +10,5 @@ def personal_best(scores):
 
 def personal_top_three(scores):
     def top_three_reducer(top, score):
-        if len(top) < 3 or score > top[-1]:
-            return sorted(top + [score], reverse=True)[:3]
-        else:
-            return top
+        return sorted(top + [score], reverse=True)[:3]
     return reduce(top_three_reducer, scores, [])
