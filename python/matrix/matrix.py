32766a065913fcbe34792438dfc581d51800a7d8
jaccarmac <jaccarmac@gmail.com>
2019-05-16 05:00:17 +0000

    Solve matrix problem in Python.
    FossilOrigin-Name: cde49186b272f681c6a1094b6adbc85c304dae3768e1fceef4a1099cc0e35dd5

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,11 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        rows = matrix_string.split('\n')
+        self.elems = [list(map(int, row.split(' '))) for row in rows]
+        
 
     def row(self, index):
-        pass
+        return self.elems[index - 1]
 
     def column(self, index):
-        pass
+        return [row[index - 1] for row in self.elems]
