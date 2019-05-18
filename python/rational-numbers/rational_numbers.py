16029720e67cae5125e9026e9adece2c9da21038
jaccarmac <jaccarmac@gmail.com>
2019-05-18 03:32:42 +0000

    Python makes rationals silly simple.
    FossilOrigin-Name: fa31076d40b64dac1ca6139fff5d569d1543d519d78be4e7a53a68d8e3a50a7e

--- python/rational-numbers/rational_numbers.py
+++ python/rational-numbers/rational_numbers.py
@@ -2,9 +2,21 @@ from __future__ import division
 
 
 class Rational(object):
+    def _common_divisor(self, x, y):
+        if x == 0: return y
+        if y == 0: return x
+        for div in range(int(min(abs(x), abs(y))), 1, -1):
+            if x % div == 0 and y % div == 0:
+                return div
+        return 1
+
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        divisor = self._common_divisor(numer, denom)
+        self.numer = numer / divisor
+        self.denom = denom / divisor
+        if self.denom < 0:
+            self.numer = -self.numer
+            self.denom = -self.denom
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -13,22 +25,24 @@ class Rational(object):
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+        return Rational(self.numer * other.denom + other.numer * self.denom,
+                        self.denom * other.denom)
 
     def __sub__(self, other):
-        pass
+        return Rational(self.numer * other.denom - other.numer * self.denom,
+                        self.denom * other.denom)
 
     def __mul__(self, other):
-        pass
+        return Rational(self.numer * other.numer, self.denom * other.denom)
 
     def __truediv__(self, other):
-        pass
+        return Rational(self.numer * other.denom, other.numer * self.denom)
 
     def __abs__(self):
-        pass
+        return Rational(abs(self.numer), abs(self.denom))
 
     def __pow__(self, power):
-        pass
+        return Rational(self.numer ** power, self.denom ** power)
 
     def __rpow__(self, base):
-        pass
+        return (base ** self.numer) ** (1 / self.denom)
