7e35594fa556bce9cf2ff8322f5a67a75d728890
jaccarmac <jaccarmac@gmail.com>
2019-05-18 07:34:07 +0000

    Wow this exercise sucked hard.
    FossilOrigin-Name: eb78869520ffd9ba522ea1a4fc8ab04d48bf71075bce0ff3b58af5c583cd9734

--- common-lisp/beer-song/beer.lisp
+++ common-lisp/beer-song/beer.lisp
@@ -4,3 +4,31 @@
   (:export #:verse #:sing))
 
 (in-package #:beer)
+
+(defun verse (bottles)
+  (let ((bottles (case bottles
+                   (0 "No more bottles")
+                   (1 "1 bottle")
+                   (t (format nil "~a bottles" bottles))))
+        (second-bottles (case bottles
+                          (0 "no more bottles")
+                          (1 "1 bottle")
+                          (t (format nil "~a bottles" bottles))))
+        (second-line (case bottles
+                       (0 "Go to the store and buy some more")
+                       (1 "Take it down and pass it around")
+                       (t "Take one down and pass it around")))
+        (next-bottles (case bottles
+                        (0 "99 bottles")
+                        (1 "no more bottles")
+                        (2 "1 bottle")
+                        (t (format nil "~a bottles" (1- bottles))))))
+    (format nil
+            "~a of beer on the wall, ~a of beer.~%~
+             ~a, ~a of beer on the wall.~%"
+            bottles second-bottles second-line next-bottles)))
+
+(defun sing (from &optional (to 0))
+  (format nil "~{~a~%~}"
+          (loop for i from from downto to
+             collect (verse i))))
