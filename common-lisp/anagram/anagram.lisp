7f22fb3d4fb3eb882870850bc7d428ad302c00d2
jaccarmac <jaccarmac@gmail.com>
2019-05-20 20:13:23 +0000

    wip
    FossilOrigin-Name: 8bc88cb12d5c0d4c5c8e5a5aed9e40a455a789dab868f2d73420fc645e0ce2f3

--- common-lisp/anagram/anagram.lisp
+++ common-lisp/anagram/anagram.lisp
@@ -4,3 +4,14 @@
   (:export #:anagrams-for))
 
 (in-package #:anagram)
+
+(defun anagram-p (word1 word2)
+  )
+
+(defun anagrams-for (word corpus)
+  (let ((word (string-downcase word))
+        (corpus (mapc #'string-downcase corpus)))
+    (loop for cword in corpus
+       when (and (not (string= cword word))
+                 (anagram-p word cword))
+       collect cword)))
