1d2fecf700afefa02b4c8cd24f440613b1bcd26c
jaccarmac <jaccarmac@gmail.com>
2019-05-25 05:46:32 +0000

    Implement Roman numeral conversion.
    FossilOrigin-Name: cd6079f44ef7e1c0079f2aff828b9b81605c879dd6961c281938504c2554e452

--- common-lisp/roman-numerals/roman.lisp
+++ common-lisp/roman-numerals/roman.lisp
@@ -3,3 +3,27 @@
   (:export #:romanize))
 
 (in-package #:roman)
+
+(defun roman-power-up (roman)
+  (map 'string
+       (lambda (digit) (ccase digit (#\I #\X)
+                                    (#\V #\L)
+                                    (#\X #\C)
+                                    (#\L #\D)
+                                    (#\C #\M)))
+       roman))
+
+(defun romanize (num)
+  (case num (0 "")
+            (1 "I")
+            (2 "II")
+            (3 "III")
+            (4 "IV")
+            (5 "V")
+            (6 "VI")
+            (7 "VII")
+            (8 "VIII")
+            (9 "IX")
+            (t (concatenate 'string
+                            (roman-power-up (romanize (floor num 10)))
+                            (romanize (mod num 10))))))
