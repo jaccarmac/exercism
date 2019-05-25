9d2b11a837976f4d00ca1a598e9d3da3674cadfa
jaccarmac <jaccarmac@gmail.com>
2019-05-25 06:50:17 +0000

    Of course there's a format directive for it.
    FossilOrigin-Name: b1566d84a0ac5beb54b4bcc33c0e6d852517911c47746c0374e13774bcb3d3f7

--- common-lisp/roman-numerals/roman.lisp
+++ common-lisp/roman-numerals/roman.lisp
@@ -4,26 +4,5 @@
 
 (in-package #:roman)
 
-(defun roman-power-up (roman)
-  (map 'string
-       (lambda (digit) (ccase digit (#\I #\X)
-                                    (#\V #\L)
-                                    (#\X #\C)
-                                    (#\L #\D)
-                                    (#\C #\M)))
-       roman))
-
 (defun romanize (num)
-  (case num (0 "")
-            (1 "I")
-            (2 "II")
-            (3 "III")
-            (4 "IV")
-            (5 "V")
-            (6 "VI")
-            (7 "VII")
-            (8 "VIII")
-            (9 "IX")
-            (t (concatenate 'string
-                            (roman-power-up (romanize (floor num 10)))
-                            (romanize (mod num 10))))))
+  (format nil "~@r" num))
