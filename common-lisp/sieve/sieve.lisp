7fedbc9309fe11e5d422edf39ba63e86b09584ae
jaccarmac <jaccarmac@gmail.com>
2019-05-26 21:44:54 +0000

    Messy sieve.
    FossilOrigin-Name: 27288847effabc8c873cce09b8b825bcf81c3da1049d0eaff358ef168bf4aac6

--- common-lisp/sieve/sieve.lisp
+++ common-lisp/sieve/sieve.lisp
@@ -4,3 +4,16 @@
   (:documentation "Generates a list of primes up to a given limit."))
 
 (in-package #:sieve)
+
+(defun primes-to (max)
+  (let ((unmarked (loop for n from 2 to max collect n))
+        (primes ()))
+    (loop while unmarked
+       do (let* ((num (car unmarked))
+                 (multiple num))
+            (setf primes (nconc primes (list num)))
+            (loop while (<= multiple max)
+               do (progn
+                    (setf unmarked (remove multiple unmarked))
+                    (setf multiple (+ multiple num))))))
+    primes))
