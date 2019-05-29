8371e725d0281b607e3500d8d62fb19281032366
jaccarmac <jaccarmac@gmail.com>
2019-05-29 21:34:10 +0000

    Factor out some duplication.
    FossilOrigin-Name: 4f144dc74cb7427cc16b5990c8f131709989032a1fed709c6d987399d2471778

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -12,8 +12,10 @@ emptyCount :: Map Nucleotide Int
 emptyCount = fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
 
 countNucleotide :: Char -> Map Nucleotide Int -> Either String (Map Nucleotide Int)
-countNucleotide 'A' = Right . insertWith (+) A 1
-countNucleotide 'C' = Right . insertWith (+) C 1
-countNucleotide 'G' = Right . insertWith (+) G 1
-countNucleotide 'T' = Right . insertWith (+) T 1
-countNucleotide c   = const $ Left [c]
+countNucleotide n  = case n of
+  'A' -> incrementKey A
+  'C' -> incrementKey C
+  'G' -> incrementKey G
+  'T' -> incrementKey T
+  c   -> const $ Left [c]
+  where incrementKey k = Right . insertWith (+) k 1
