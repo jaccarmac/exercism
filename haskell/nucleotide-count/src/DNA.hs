1d67cbedc99ea47355f04e2a1bab6136870c6970
jaccarmac <jaccarmac@gmail.com>
2019-05-29 21:23:26 +0000

    Initial count solution in Haskell.
    FossilOrigin-Name: b659538a26a51a88c9642249d0389b6b12d6e7589fdc0001560456128e260dfb

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,16 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import           Control.Monad (foldM)
+import           Data.Map      (Map, fromList, insertWith)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts = foldM countNucleotide $ fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+
+countNucleotide :: Map Nucleotide Int -> Char -> Either String (Map Nucleotide Int)
+countNucleotide m 'A' = Right $ insertWith (+) A 1 m
+countNucleotide m 'C' = Right $ insertWith (+) C 1 m
+countNucleotide m 'G' = Right $ insertWith (+) G 1 m
+countNucleotide m 'T' = Right $ insertWith (+) T 1 m
+countNucleotide m c   = Left [c]
