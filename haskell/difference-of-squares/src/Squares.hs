373b7e78b1011700d46e40d8030fb8b70201be30
jaccarmac <jaccarmac@gmail.com>
2019-05-30 21:07:57 +0000

    Custom square function, remove warnings.
    FossilOrigin-Name: e3ba1c54111d425d49db96b3840a1ac54eeddeb7c7dc226d6734480355c9a935

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -1,12 +1,13 @@
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
-
 module Squares (difference, squareOfSum, sumOfSquares) where
 
 difference :: Integral a => a -> a
 difference n = squareOfSum n - sumOfSquares n
 
 squareOfSum :: Integral a => a -> a
-squareOfSum n = sum [1..n] ^ 2
+squareOfSum n = square $ sum [1..n]
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum $ map (^2) [1..n]
+sumOfSquares n = sum $ map square [1..n]
+
+square :: Integral a => a -> a
+square n = n * n
