782ae1293d6a6ac4bebc2f2bd8a08eacd97420ae
jaccarmac <jaccarmac@gmail.com>
2019-06-05 17:13:30 +0000

    Recursive/naive solution.
    FossilOrigin-Name: 2bae7f3ded34f0df0864ba1f8488a1299113a8522537119a9527b8c1ce38784e

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum $ filter (isMultiple factors) [1..limit-1]
+
+isMultiple :: [Integer] -> Integer -> Bool
+isMultiple [] _     = False
+isMultiple (0:fs) n = isMultiple fs n
+isMultiple (f:fs) n = n `mod` f == 0 || isMultiple fs n
