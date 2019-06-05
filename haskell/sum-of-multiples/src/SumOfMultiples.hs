c08f39d3b9699f3816b23f5e4d8f2d4f3fca133c
jaccarmac <jaccarmac@gmail.com>
2019-06-05 17:56:30 +0000

    More mathematical solution.
    FossilOrigin-Name: 4ddefc07b35efa0e04997d2be2217328f1850695a33cd7419acae51fc53f85a8

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,9 +1,7 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ filter (isMultiple factors) [1..limit-1]
+sumOfMultiples factors limit = sum $ filter (isMultipleOf factors) [1..limit-1]
 
-isMultiple :: [Integer] -> Integer -> Bool
-isMultiple [] _     = False
-isMultiple (0:fs) n = isMultiple fs n
-isMultiple (f:fs) n = n `mod` f == 0 || isMultiple fs n
+isMultipleOf :: [Integer] -> Integer -> Bool
+isMultipleOf factors n = any (\m -> m /= 0 && n `mod` m == 0) factors
