29ee83a7248c52a6ac8ac8c1d3a82a0c6bc7f832
jaccarmac <jaccarmac@gmail.com>
2019-06-07 08:59:01 +0000

    Remove deepseq, minor dep and text updates.
    FossilOrigin-Name: 29043ae0879e0cc4f0e52553d57db7420fdffcddc88edb66423b3b77daad187c

--- haskell/ghc-validation.scm
+++ haskell/ghc-validation.scm
@@ -5,37 +5,6 @@
              (gnu packages haskell)
              (gnu packages haskell-check))
 
-(define ghc-deepseq
-  (package
-   (name "ghc-deepseq")
-   (version "1.4.4.0")
-   (source (origin
-            (method url-fetch)
-            (uri (string-append "https://hackage.haskell.org/package/"
-                                "deepseq/deepseq-"
-                                version ".tar.gz"))
-            (sha256
-             (base32 "09kfpmgl679l74b6dadia11pvhya9ik4wrd8x76cgkxk7gwcbkrc"))))
-   (build-system haskell-build-system)
-   (inputs
-    `(("ghc-hunit" ,ghc-hunit)
-      ("ghc-test-framework-hunit" ,ghc-test-framework-hunit)))
-   (home-page "https://github.com/haskell/deepseq")
-   (synopsis "Deep evaluation of data structures")
-   (description
-    "This package provides methods for fully evaluating data structures (\"deep
-evaluation\"). Deep evaluation is often used for adding strictness to a
-program, e.g. in order to force pending exceptions, remove space leaks, or
-force lazy I/O to happen. It is also useful in parallel programs, to ensure
-pending work does not migrate to the wrong thread.
-
-The primary use of this package is via the deepseq function, a \"deep\" version
-of seq. It is implemented on top of an NFData typeclass (\"Normal Form Data\",
-data structures with no unevaluated components) which defines strategies for
-fully evaluating different data types. See module documentation in
-Control.DeepSeq for more details.")
-   (license license:bsd-3)))
-
 (define ghc-ansi-terminal-newer
   (package (inherit ghc-ansi-terminal)
    (name "ghc-ansi-terminal")
@@ -64,9 +33,10 @@ Control.DeepSeq for more details.")
              (base32 "1wnjxnwbc3l853kiiijagzjyb6fmhz3lmkwls24plbximl1qrr22"))))
    (build-system haskell-build-system)
    (inputs
-    `(("ghc-ansi-terminal" ,ghc-ansi-terminal-newer)
-      ("ghc-async" ,ghc-async)
+    `(("ghc-async" ,ghc-async)
+      ("ghc-stm" ,ghc-stm)
       ("ghc-exceptions" ,ghc-exceptions)
+      ("ghc-ansi-terminal" ,ghc-ansi-terminal-newer)
       ("ghc-terminal-size" ,ghc-terminal-size)))
    (home-page "https://hackage.haskell.org/package/concurrent-output")
    (synopsis "Ungarble output from several threads or commands")
@@ -91,8 +61,8 @@ concurrent threads. Can be used for progress displays etc.")
             (sha256
              (base32 "1br7qyf27iza213inwhf9bm2k6in0zbmfw6w4clqlc9f9cj2nrkb"))))
    (build-system haskell-build-system)
-   (inputs
-    `(("ghc-hunit" ,ghc-hunit)
+   (native-inputs
+    `(("ghc-tasty" ,ghc-tasty)
       ("ghc-tasty-hunit" ,ghc-tasty-hunit)))
    (home-page "https://github.com/minad/wl-pprint-annotated#readme")
    (synopsis "Pretty printer with annotation support")
@@ -115,6 +85,7 @@ API. Annotations are useful for coloring. See wl-pprint-console.")
    (build-system haskell-build-system)
    (inputs
     `(("ghc-ansi-terminal" ,ghc-ansi-terminal)
+      ("ghc-async" ,ghc-async)
       ("ghc-concurrent-output" ,ghc-concurrent-output)
       ("ghc-exceptions" ,ghc-exceptions)
       ("ghc-lifted-async" ,ghc-lifted-async)
@@ -125,18 +96,19 @@ API. Annotations are useful for coloring. See wl-pprint-console.")
       ("ghc-random" ,ghc-random)
       ("ghc-resourcet" ,ghc-resourcet)
       ("ghc-semigroups" ,ghc-semigroups)
+      ("ghc-stm" ,ghc-stm)
       ("ghc-th-lift" ,ghc-th-lift)
       ("ghc-transformers-base" ,ghc-transformers-base)
       ("ghc-wl-pprint-annotated" ,ghc-wl-pprint-annotated)))
    (home-page "https://hedgehog.qa")
-   (synopsis "Hedgehog will eat all your bugs")
+   (synopsis "Hedgehog will eat all your bugs.")
    (description
     "Hedgehog is a modern property-based testing system, in the spirit of
 QuickCheck. Hedgehog uses integrated shrinking, so shrinks obey the invariants
 of generated values by construction.
 
 To get started quickly, see the examples:
-https://github.com/hedgehogqa/haskell-hedgehog/tree/master/hedgehog-example")
+<https://github.com/hedgehogqa/haskell-hedgehog/tree/master/hedgehog-example>")
    (license license:bsd-3)))
 
 (define-public ghc-validation
@@ -162,28 +134,28 @@ https://github.com/hedgehogqa/haskell-hedgehog/tree/master/hedgehog-example")
                                       (format #t "import Distribution.Simple~%")
                                       (format #t "main = defaultMain~%"))))))))
    (inputs
-    `(("ghc-bifunctors" ,ghc-bifunctors)
-      ("ghc-deepseq" ,ghc-deepseq)
-      ("ghc-lens" ,ghc-lens)
+    `(("ghc-semigroups" ,ghc-semigroups)
       ("ghc-semigroupoids" ,ghc-semigroupoids)
-      ("ghc-semigroups" ,ghc-semigroups)
-      ("ghc-hunit" ,ghc-hunit)
-      ("ghc-hedgehog" ,ghc-hedgehog)))
+      ("ghc-bifunctors" ,ghc-bifunctors)
+      ("ghc-lens" ,ghc-lens)))
+   (native-inputs
+    `(("ghc-hedgehog" ,ghc-hedgehog)
+      ("ghc-hunit" ,ghc-hunit)))
    (home-page "https://github.com/qfpl/validation")
    (synopsis "A data-type like Either but with an accumulating Applicative")
    (description
-    "This package provides data-type like Either but with differing properties
-and type-class instances.
+    "A data-type like Either but with differing properties and type-class
+instances.
 
 Library support is provided for this different representation, include
-lens-related functions for converting between each and abstracting over their
+`lens`-related functions for converting between each and abstracting over their
 similarities.
 
-The Validation data type is isomorphic to Either, but has an instance of
-Applicative that accumulates on the error side. That is to say, if two (or
-more) errors are encountered, they are appended using a Semigroup operation.
+The `Validation` data type is isomorphic to `Either`, but has an instance of
+`Applicative` that accumulates on the error side. That is to say, if two (or
+more) errors are encountered, they are appended using a `Semigroup` operation.
 
-As a consequence of this Applicative instance, there is no corresponding Bind
-or Monad instance. Validation is an example of, \"An applicative functor that
-is not a monad.\"")
+As a consequence of this `Applicative` instance, there is no corresponding
+`Bind` or `Monad` instance. `Validation` is an example of, \"An applicative
+functor that is not a monad.\"")
    (license license:bsd-3)))
