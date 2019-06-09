740e3e29445af1f9348f018309c8d15c86b0a130
jaccarmac <jaccarmac@gmail.com>
2019-06-09 16:54:41 +0000

    Simpler grain on square n calc.
    FossilOrigin-Name: 6c72778dcad61a6e92e625a18b852087299b4c7b6e2c66f59da493831a5bfa3f

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -5,8 +5,7 @@ import           Control.Applicative (liftA2)
 square :: Integer -> Maybe Integer
 square n | n < 1 = Nothing
          | n > 64 = Nothing
-square 1 = Just 1
-square n = (*2) <$> square (n - 1)
+         | otherwise = pure $ 2 ^ (n - 1)
 
 total :: Integer
 total = case foldl (liftA2 (+)) (Just 0) $ map square [1..64] of
