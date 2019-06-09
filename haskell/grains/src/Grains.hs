a47f2728fe7d3bfdbf6998022dab5b81243d7852
jaccarmac <jaccarmac@gmail.com>
2019-06-09 17:38:34 +0000

    Even shorter.
    FossilOrigin-Name: 62a53920ecdc1c4ab66995bbd3191b050a90e71440c000562a0a4af7dc7b01b4

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,11 +1,9 @@
 module Grains (square, total) where
 
-import           Data.Maybe (fromMaybe)
-
 square :: Integer -> Maybe Integer
 square n | n < 1 = Nothing
          | n > 64 = Nothing
          | otherwise = pure $ 2 ^ (n - 1)
 
 total :: Integer
-total = fromMaybe 0 $ sum <$> traverse square [1..64]
+total = maybe 0 sum $ traverse square [1..64]
