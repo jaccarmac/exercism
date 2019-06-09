8bdc3dc981551e92ed0f3d2f17df80109b354804
jaccarmac <jaccarmac@gmail.com>
2019-06-09 00:25:03 +0000

    Add initial grains solution.
    FossilOrigin-Name: 527d05f163daf231034c3c2ebc4607e2e11090b384da239ba8b9bf6297f63df8

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,14 @@
 module Grains (square, total) where
 
+import           Control.Applicative (liftA2)
+
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n | n < 1 = Nothing
+         | n > 64 = Nothing
+square 1 = Just 1
+square n = (*2) <$> square (n - 1)
 
 total :: Integer
-total = error "You need to implement this function."
+total = case foldl (liftA2 (+)) (Just 0) $ map square [1..64] of
+  Nothing -> 0
+  Just n  -> n
