c66703e844528756d8a54b93e953c9bd94a237d9
jaccarmac <jaccarmac@gmail.com>
2019-07-24 22:26:13 +0000

    Add relatively-faithful translation of CL Bob for Clojure.
    FossilOrigin-Name: 83fc0553932a1adbe413ab74f3b5ce579126c42cb5068e4e79c73831bce7b710

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,21 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn asking? [input]
+  (= \? (last input)))
+
+(defn yelling? [input]
+  (and (some #(Character/isLetter %) input)
+       (= input (clojure.string/upper-case input))))
+
+(defn saying-nothing? [input]
+  (= "" input))
+
+(defn response-for [s]
+  (let [input (clojure.string/trim s)]
+    (cond (saying-nothing? input) "Fine. Be that way!"
+          (and (asking? input) (yelling? input))
+          "Calm down, I know what I'm doing!"
+          (asking? input) "Sure."
+          (yelling? input) "Whoa, chill out!"
+          :else "Whatever.")))
