175f75f4d6882028c437b17eb46ca53200e4be7d
jaccarmac <jaccarmac@gmail.com>
2019-08-25 10:33:26 +0000

    Only go through the string once.
    FossilOrigin-Name: 73fc7b8a3863f545213e61c048649b2e652f83afccc9e825bf96cde659d33ccd

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,11 +1,15 @@
 (ns rna-transcription
   (:require [clojure.set :refer [union]]))
 
-(defn dna? [dna]
-  (let [valid #{\G \T \A \C}]
-    (= valid (union valid (set dna)))))
+(def complement
+  {\G \C
+   \T \A
+   \A \U
+   \C \G})
 
 (defn to-rna [dna]
-  (if-not (dna? dna)
-    (throw (AssertionError. "Bad nucleotide."))
-    (apply str (map {\G \C, \T \A, \A \U, \C \G} dna))))
+  (apply str (reduce #(if-let [n (complement %2)]
+                        (conj %1 n)
+                        (throw (AssertionError. "Bad nucleotide.")))
+                     []
+                     dna)))
