a9bfe8b9458657c06f3940dd0d49e01b1ef7e3d5
jaccarmac <jaccarmac@gmail.com>
2019-08-25 10:20:19 +0000

    RNA in Clojure.
    FossilOrigin-Name: e80d8c6c85bfddb538d6325ea1dcc63278479ee590c330dce555fdfec874efe5

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,11 @@
-(ns rna-transcription)
+(ns rna-transcription
+  (:require [clojure.set :refer [union]]))
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn dna? [dna]
+  (let [valid #{\G \T \A \C}]
+    (= valid (union valid (set dna)))))
+
+(defn to-rna [dna]
+  (if-not (dna? dna)
+    (throw (AssertionError. "Bad nucleotide."))
+    (apply str (map {\G \C, \T \A, \A \U, \C \G} dna))))
