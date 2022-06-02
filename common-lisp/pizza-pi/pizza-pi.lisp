eb0c534f17ef88f7056050cb82f0bed530a4f241
jaccarmac <jaccarmac@gmail.com>
2022-06-02 14:09:59 +0000

    Implement the pizza solutions with various math functions.
    FossilOrigin-Name: 2838b1c6566dc0c7f26b90b965762d3b8c62d62ce32502564798ecae0d982966

--- common-lisp/pizza-pi/pizza-pi.lisp
+++ common-lisp/pizza-pi/pizza-pi.lisp
@@ -5,10 +5,14 @@
 
 (in-package :pizza-pi)
 
-(defun dough-calculator (pizzas diameter))
+(defun dough-calculator (pizzas diameter)
+  (round (* pizzas (+ 200 (/ (* 45 pi diameter) 20)))))
 
-(defun size-from-sauce (sauce))
+(defun size-from-sauce (sauce)
+  (sqrt (/ (* 40 sauce) (* 3 pi))))
 
-(defun pizzas-per-cube (cube-size diameter))
+(defun pizzas-per-cube (cube-size diameter)
+  (floor (/ (* 2 (expt cube-size 3)) (* 3 pi (expt diameter 2)))))
 
-(defun fair-share-p (pizzas friends))
+(defun fair-share-p (pizzas friends)
+  (zerop (mod (* 8 pizzas) friends)))
