169c53a7f8bd9faadd83e519ea3c4de34c7f92bc
jaccarmac <jaccarmac@gmail.com>
2022-06-03 17:51:46 +0000

    Implement shamelessly using the stolen matrix check.
    FossilOrigin-Name: e24142c5b67a4b95c8f8325ae23cf9b13bd384b037b9d9d7f9af31c95b83209e

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -1,29 +1,9 @@
 module Diamond (diamond) where
 
-import           Data.Char (chr, ord)
-
 diamond :: Char -> Maybe [String]
 diamond c | c < 'A' || c > 'Z' = Nothing
-          | otherwise = pure $ mirrorRows $ padRows (diamondRowsFrom c) maxDepth
-  where maxDepth = ord c - ord 'A'
-
-mirrorRows :: [String] -> [String]
-mirrorRows []     = []
-mirrorRows [s]    = [s]
-mirrorRows (s:ss) = s:mirrorRows ss ++ [s]
-
-padRows :: [String] -> Int -> [String]
-padRows [] _ = []
-padRows ss 0 = ss
-padRows (s:ss) depth = (pad ++ s ++ pad):padRows ss (depth - 1)
-  where pad = replicate depth ' '
-
-diamondRowsFrom :: Char -> [String]
-diamondRowsFrom 'A' = ["A"]
-diamondRowsFrom c = diamondRowsFrom (chr $ ord c - 1) ++ [currentRow]
-  where currentRow = diamondRow c $ ord c - ord 'A'
-
-diamondRow :: Char -> Int -> String
-diamondRow c 0     = [c]
-diamondRow c depth = c:replicate spaces ' ' ++ [c]
-  where spaces = depth * 2 - 1
+          | otherwise = pure [[charAt x y | x <- row] | y <- column]
+          where row = [c, pred c .. 'A'] ++ ['B' .. c]
+                column = ['A' .. c] ++ tail [c, pred c .. 'A']
+                charAt x y | x == y = x
+                           | otherwise = ' '
