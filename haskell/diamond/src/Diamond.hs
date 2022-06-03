3ea527b13c4bc70d4a7f2d7a99029a6781ff770d
jaccarmac <jaccarmac@gmail.com>
2022-06-03 18:28:43 +0000

    Add possibly-confusing infix operators.
    FossilOrigin-Name: 8e4d132fa95a8151741b33f14c34ec9ea445ce4ce50cba0ae07781fabe7a3153

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -1,8 +1,10 @@
 module Diamond (diamond) where
 
+import           Data.Functor ((<&>))
+
 diamond :: Char -> Maybe [String]
 diamond c | c < 'A' || c > 'Z' = Nothing
-          | otherwise = pure [[charAt x y | x <- row] | y <- column]
+          | otherwise = pure $ (row <&>) . charAt <$> column
           where row = [c, pred c .. 'A'] ++ ['B' .. c]
                 column = ['A' .. c] ++ tail [c, pred c .. 'A']
                 charAt x y | x == y = x
